11a70215bc9221a79f6513ccc6679bb1076cc599
0880 <98263509+0880880@users.noreply.github.com>
2025-04-22 13:21:59 +0330

    Possible Fix

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
+# setup.py
 import glob
 from setuptools import find_packages, setup
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
+class bdist_wheel(_bdist_wheel):
+    def finalize_options(self):
+        super().finalize_options()
+        # force platform tag
+        self.root_is_pure = False
 
 external_files = [
     f.replace("pythonode/", "", 1)
     for f in glob.glob("pythonode/external/**/*", recursive=True)
     if not f.endswith("/")
 ]
- 
+
 setup(
     name="pythonode",
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
-    package_data={
-        "pythonode": ["lib/*", *external_files],
-    },
-    zip_safe=False,  
+    package_data={"pythonode": ["lib/*", *external_files]},
+    zip_safe=False,
+    cmdclass={"bdist_wheel": bdist_wheel},
 )
