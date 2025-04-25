dda4210bcdbe4c3043e27e887cd054fdbf77ccbc
0880 <98263509+0880880@users.noreply.github.com>
2025-04-25 20:23:06 +0330

    Test

--- pre_build.sh
+++ pre_build.sh
@@ -6,16 +6,14 @@ if [[ "$(uname)" == "Linux" ]]; then
 
     echo "🧰 Installing Clang..."
     yum install -y curl xz
-    curl -SL https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz | tar -xJ
-    mv clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04 /opt/clang_10
+    curl -SL https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-centos7.tar.xz \
+      | tar -xJ
+
+    mv clang+llvm-10.0.0-x86_64-linux-gnu-centos7 /opt/clang_10
     export PATH=/opt/clang_10/bin:$PATH
     export LD_LIBRARY_PATH=/opt/clang_10/lib:$LD_LIBRARY_PATH
 
-    export CC=clang
-    export CXX=clang++
-
-    yum install -y devtoolset-9
-    source /opt/rh/devtoolset-9/enable
+    clang++ --version
 
     echo "⬆️  Updating pip, setuptools, and wheel..."
     python -m pip install --upgrade pip setuptools wheel
