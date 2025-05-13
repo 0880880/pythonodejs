a4d3c7945df56594686ba95669a168792564a632
0880 <98263509+0880880@users.noreply.github.com>
2025-05-13 18:56:26 +0000

    Remove semantic-release

--- .github/workflows/ver.py
+++
@@ -1,1 +0,0 @@
-import os,re;f='pyproject.toml';v=os.getenv('VERSION');t=open(f).read();open(f,'w').write(re.sub(r'(\[project\][\s\S]*?version\s*=\s*)\"[^\"]*\"', fr'\1\"{v}\"', t, flags=re.M))
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ ext = Extension(
 
 setup(
     name="pythonodejs",
-    version="0.1.3",
+    version="1.0.1",
     packages=find_packages(),
     ext_modules=[ext],
     include_package_data=True,
