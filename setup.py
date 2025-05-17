74c1462cdae4a1c85423b801e06b4e8c118f3bfb
0880 <98263509+0880880@users.noreply.github.com>
2025-05-17 17:16:05 +0330

    Release 1.0.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ ext = Extension(
 
 setup(
     name="pythonodejs",
-    version="1.0.4",
+    version="1.0.5",
     packages=find_packages(),
     ext_modules=[ext],
     include_package_data=True,
