d80aa5d150f198c6ccd4face35dd857e1ae9fc64
0880 <98263509+0880880@users.noreply.github.com>
2025-06-02 21:13:01 +0330

    Version 1.3.0

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ import os
 from setuptools import find_packages, setup, Extension
 from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
+
 class bdist_wheel(_bdist_wheel):
     def finalize_options(self):
         super().finalize_options()
         self.root_is_pure = False
+
 
 external_files = [
     f.replace("pythonodejs/", "", 1)
@@ -23,22 +25,19 @@ ext = Extension(
 
 setup(
     name="pythonodejs",
-    version="1.2.0",
+    version="1.3.0",
     packages=find_packages(),
     ext_modules=[ext],
     include_package_data=True,
-    python_requires='>=3.9,<=3.11',
-    package_data = {
+    python_requires=">=3.9,<=3.11",
+    package_data={
         "pythonodejs": [
             *external_files,
             *[
-                f"lib/{f}" for f in os.listdir("pythonodejs/lib")
-                if not (
-                    f.endswith(".exp") or
-                    f.endswith(".pdb") or
-                    f.endswith(".ilk")
-                )
-            ]
+                f"lib/{f}"
+                for f in os.listdir("pythonodejs/lib")
+                if not (f.endswith(".exp") or f.endswith(".pdb") or f.endswith(".ilk"))
+            ],
         ]
     },
     zip_safe=False,
