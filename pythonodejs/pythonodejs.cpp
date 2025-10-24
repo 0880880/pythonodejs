d5bcc1e58c76f651b461f8a5e0bf51fb98fba345
0880 <98263509+0880880@users.noreply.github.com>
2025-10-24 20:23:56 +0330

    Fix namespace

--- pythonodejs/pythonodejs.cpp
+++ pythonodejs/pythonodejs.cpp
@@ -218,11 +218,11 @@ NodeEnv* NodeEnvCreate(const char* absolute_path)
     node->loop = setup->event_loop();
 
     {
-        Locker locker(isolate);
-        Isolate::Scope isolate_scope(isolate);
-        HandleScope handle_scope(isolate);
-        Local<Context> context = setup->context();
-        Context::Scope context_scope(setup->context());
+        v8::Locker locker(isolate);
+        v8::Isolate::Scope isolate_scope(isolate);
+        v8::HandleScope handle_scope(isolate);
+        Local<v8::Context> context = setup->context();
+        v8::Context::Scope context_scope(setup->context());
         string import_name = "import_" + random_string(6);
         MaybeLocal<Value> ret = _nodejs::LoadEnvironment(env,
             "function " + import_name + "(s) { return import(s); }"
