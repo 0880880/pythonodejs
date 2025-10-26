f0c4210742531860c0cec466e2a09923c3549cfb
0880 <98263509+0880880@users.noreply.github.com>
2025-10-26 13:29:51 +0330

    Remove unnecessary scripts

--- scripts/dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-bear -- clang++ -c -std=c++20 -DNODE_WANT_INTERNALS=1 -shared -fPIC pythonodejs/pythonodejs.cpp \
-    -Iinclude/node/src     -Iinclude/node/deps/v8/include -Iinclude/node/deps/uv/include \
-    -o node$(python3-config --extension-suffix) \
-    -Llibs/libnode -Wl,-rpath=$(pwd)/libs/libnode -lnode \
-    $(python3-config --includes --ldflags)
