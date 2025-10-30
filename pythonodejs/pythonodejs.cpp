a242056dc4e7b4533059ff3f89a9c4efbaf1edc2
0880 <98263509+0880880@users.noreply.github.com>
2025-10-30 16:30:45 +0330

    Fix array and tuple indexing

--- pythonodejs/pythonodejs.cpp
+++ pythonodejs/pythonodejs.cpp
@@ -465,7 +465,7 @@ Local<Value> PyToJS(NodeEnv* node, PyObject* value)
         int len = PyList_Size(value);
         Local<Array> arr = Array::New(node->isolate, len);
         for (int i = 0; i < len; i++) {
-            arr->Set(context, i, PyToJS(node, PyList_GetItem(value, len))).Check();
+            arr->Set(context, i, PyToJS(node, PyList_GetItem(value, i))).Check();
         }
         return arr;
     } else if (PyTuple_Check(value)) // Array
@@ -473,7 +473,7 @@ Local<Value> PyToJS(NodeEnv* node, PyObject* value)
         int len = PyTuple_Size(value);
         Local<Array> arr = Array::New(node->isolate, len);
         for (int i = 0; i < len; i++) {
-            arr->Set(context, i, PyToJS(node, PyTuple_GetItem(value, len))).Check();
+            arr->Set(context, i, PyToJS(node, PyTuple_GetItem(value, i))).Check();
         }
         return arr;
     } else if (PyDict_Check(value)) // Object
