5936bcd4b734a38e70b130c884500cfe8741882a
0880 <98263509+0880880@users.noreply.github.com>
2025-11-26 22:52:52 +0330

    Free BigInt words

--- src/pythonodejs/pythonodejs.cpp
+++ src/pythonodejs/pythonodejs.cpp
@@ -566,7 +566,9 @@ MaybeLocal<Value> PyToJS(NodeEnv* node, PyObject* value)
             }
             int sign = _PyLong_Sign(value) == -1 ? 1 : 0;
             using v8::BigInt;
-            return BigInt::NewFromWords(context, sign, nwords, words).ToLocalChecked();
+            MaybeLocal<BigInt> b = BigInt::NewFromWords(context, sign, nwords, words);
+            free(words);
+            return b;
         } else if (num < -2147483648LL || num > 2147483647LL) {
             using v8::BigInt;
             return BigInt::New(node->isolate, num);
