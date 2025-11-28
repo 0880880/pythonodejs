5a506787c21095fd374d9d37ec8511dcff7b5872
0880 <98263509+0880880@users.noreply.github.com>
2025-11-29 01:32:27 +0330

    Create python event loop driver

--- src/pythonodejs/__init__.py
+++ src/pythonodejs/__init__.py
@@ -1,8 +1,9 @@
 from ._pythonodejs import NodeJS
+from ._event_loop import event_loop_driver
 
 try:
     from ._version import version as __version__  # type: ignore
 except ImportError:
     __version__ = "unknown"
 
-__all__ = ["__version__", "NodeJS"]
+__all__ = ["__version__", "NodeJS", "event_loop_driver"]
---
+++ src/pythonodejs/event_loop.py
@@ -0,0 +1,9 @@
+import asyncio
+import _pythonodejs
+
+
+async def event_loop_driver(node_instance):
+    while True:
+        is_alive = node_instance.poll()
+
+        await asyncio.sleep(0)
